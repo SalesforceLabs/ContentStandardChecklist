2b2a21803a74bb0238c37bdb5e93a6c8e996204e
Andrea Morales <amorales@altimetrik.com>
2018-01-25 15:48:00 -0300

    #586

--- src/aura/aqi_Toast/aqi_ToastController.js
+++ src/aura/aqi_Toast/aqi_ToastController.js
@@ -1,5 +1,9 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	closeWindow : function(component, event, helper) {
+		console.log(' close windos slds-hide ');
+		component.set("v.title",'');
+		component.set("v.description",'');
+		component.set("v.className",'slds-hide');
+		component.set("v.severity",'info'); 
 	}
 })

7b28af124b07ac1bf09d5360176880a392d39596
faldecosea <faldecosea@altimetrik.com>
2018-01-26 14:40:07 -0300

    Resolved #590 - [BUG] 'Coaching Provided By' is no longer required

--- src/aura/aqi_Wrapper/aqi_WrapperController.js
+++ src/aura/aqi_Wrapper/aqi_WrapperController.js
@@ -23,28 +23,44 @@
 			apiNames.push(component.get("v.indexField10"));
 			console.log('all api names ');
 			console.log(apiNames);
- 
+
 			component.set("v.apiNames",apiNames);
 			helper.doInit(component, event, helper);
 			console.log('do Init');
 	},
 	updateAQI : function(component, event, helper) {
 		var aqi_obj = component.get('v.aqi_record');
-		
+
 		var indexInputs = helper.getIndexInputs(component);
         if(!indexInputs) return;
+
+		var actionNeeded = component.find('Action_Needed__c').get('v.value');
+		var asignedTo = component.find('Action_Assigned_To__c').get('v.value');
 
         for(var i = 0; i < indexInputs.length; i++){
 			var aqiIndex = indexInputs[i];
 			aqi_obj[aqiIndex.get("v.fieldName")] = aqiIndex.get("v.fieldValue");
+
 			console.log('updateAQI '+aqiIndex.get("v.fieldName")+':'+aqiIndex.get("v.fieldValue"));
 		}
-		console.log('updatedAQI');
-		
-		console.log(aqi_obj);
-		component.set('v.aqi_record',aqi_obj)
-		helper.doUpdate(component);
+		console.log('asignedTo: ' + asignedTo );
+		console.log('actionneeded: ' + actionNeeded);
+		if(actionNeeded && (asignedTo === undefined || asignedTo === '')){
+			var toastCmp =  component.find("toastNotif");
+			toastCmp.set("v.title",'Error');
+			toastCmp.set("v.description",'Please specify coaching provided by');
+			toastCmp.set("v.className",'');
+			toastCmp.set("v.severity",'warning');
+		}
+		else{
+			console.log('updatedAQI');
+
+			console.log(aqi_obj);
+			component.set('v.aqi_record',aqi_obj)
+			helper.doUpdate(component);
+		}
+
 	}
-	
+
 
 })

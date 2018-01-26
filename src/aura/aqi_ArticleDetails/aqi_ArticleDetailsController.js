a68eefa55e1ddcebb78337bb54fe5901ae30e1da
Andrea Morales <amorales@altimetrik.com>
2018-01-26 16:05:00 -0300

    resolved #574

--- src/aura/aqi_ArticleDetails/aqi_ArticleDetailsController.js
+++ src/aura/aqi_ArticleDetails/aqi_ArticleDetailsController.js
@@ -1,5 +1,35 @@
 ({
 	myAction : function(component, event, helper) {
-		
-	}
+        
+    },
+    
+    doInit: function(cmp, event, helper) {
+     
+        var helper = this;
+        var action = cmp.get("c.loadKAVId");
+
+        action.setParams({ recordId: cmp.get("v.recordId")});
+        action.setCallback(this, function(response) {
+            if(!cmp.isValid()) return;
+            var state = response.getState();
+
+            if (state === "SUCCESS") {
+                //Fire updated event
+                var responseMap = response.getReturnValue();
+				console.log(responseMap);
+              	cmp.set("v.kavId",responseMap);
+
+            }
+			else if (state === "INCOMPLETE") {
+			   // do something
+		   }
+		   else if (state === "ERROR") {
+			   var errors = response.getError();
+			   console.log(errors);
+		   }
+        });
+        $A.enqueueAction(action);
+        
+        
+    }
 })

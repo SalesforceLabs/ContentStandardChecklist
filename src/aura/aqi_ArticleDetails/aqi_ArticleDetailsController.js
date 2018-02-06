48c2e61184323d53f10e5568e757ed25f4ce9cbd
faldecosea <faldecosea@altimetrik.com>
2018-02-06 10:57:55 -0300

    Resolved #618 - Refactor article detail extending abstract class

--- src/aura/aqi_ArticleDetails/aqi_ArticleDetailsController.js
+++ src/aura/aqi_ArticleDetails/aqi_ArticleDetailsController.js
@@ -4,29 +4,8 @@
     },
 
     doInit: function(cmp, event, helper) {
-        var action = cmp.get("c.loadKAVId");
 
-        action.setParams({ recordId: cmp.get("v.recordId")});
-        action.setCallback(this, function(response) {
-            if(!cmp.isValid()) return;
-            var state = response.getState();
-
-            if (state === "SUCCESS") {
-                var responseMap = response.getReturnValue();
-              	cmp.set("v.kavId",responseMap);
-				helper.afterInit(cmp,event,helper);
-
-            }
-			else if (state === "INCOMPLETE") {
-			   // do something
-		   }
-		   else if (state === "ERROR") {
-			   var errors = response.getError();
-			   console.log(errors);
-		   }
-        });
-        $A.enqueueAction(action);
-
+        helper.doInit(cmp, event, helper);
 
     }
 })
--- src/aura/aqi_ArticleDetails/aqi_ArticleDetailsHelper.js
+++ src/aura/aqi_ArticleDetails/aqi_ArticleDetailsHelper.js
@@ -1,18 +1,27 @@
 ({
+	doInit : function (cmp, event, helper){
+		var actionParams ={	recordId: cmp.get("v.recordId")};
+		this.handleAction(cmp, actionParams, 'c.loadKAVId', this.doInitCallback);
+	},
+	doInitCallback : function( cmp, response, ctx) {
+		if(!cmp.isValid()) return;
+		var responseMap = response;
+		cmp.set("v.kavId",responseMap);
+		ctx.afterInit(cmp,event,ctx);
+	},
 	afterInit: function(cmp, event, helper)
-	{
-		var actionIsArchived = cmp.get("c.kavIsArchived");
-		actionIsArchived.setParams({ kavIdParameter:  cmp.get("v.kavId")});
-		actionIsArchived.setCallback(this, function(response) {
-			var responseBool = response.getReturnValue();
-			if(responseBool){
-				var toastCmp =  cmp.find("toastNotif");
-				toastCmp.set("v.title",'WARNING');
-				toastCmp.set("v.description",'The related Knowledge Article is archived.');
-				toastCmp.set("v.className",'');
-				toastCmp.set("v.severity",'warning'); 
-			}
-		});
-		$A.enqueueAction(actionIsArchived);
+	{ 
+		var actionParams ={	kavIdParameter: cmp.get("v.kavId")};
+		this.handleAction(cmp, actionParams, 'c.kavIsArchived', this.afterInitCallback);
+	},
+	afterInitCallback : function(cmp, responseMap, ctx){
+		var responseBool = responseMap;
+		if(responseBool){
+			var toastCmp =  cmp.find("toastNotif");
+			toastCmp.set("v.title",'WARNING');
+			toastCmp.set("v.description",'The related Knowledge Article is archived.');
+			toastCmp.set("v.className",'');
+			toastCmp.set("v.severity",'warning');
+		}
 	}
 })

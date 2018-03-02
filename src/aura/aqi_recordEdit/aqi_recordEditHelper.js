a2698adeafc7bff089427477411fb5f00ec36ddd
faldecosea <faldecosea@altimetrik.com>
2018-03-02 14:59:23 -0300

    Resolved #655 - Article fields on AQI record, like published date, should not be editable - Remove aqi_KnowledgeSearchV2 page and update package

--- src/aura/aqi_recordEdit/aqi_recordEditHelper.js
+++ src/aura/aqi_recordEdit/aqi_recordEditHelper.js
@@ -1,17 +1,6 @@
 ({
 	backToRecord : function(component) {
-       /* var urlEvent = $A.get("e.force:navigateToURL");
-        urlEvent.setParams({
-            "url": "/"+component.get("v.recordId")
-        });
-        urlEvent.fire();*/
-        
-        var navEvt = $A.get("e.force:navigateToSObject");
-        navEvt.setParams({
-          "recordId": component.get("v.recordId")
-        });
-        navEvt.fire();
-        //$A.get('e.force:refreshView').fire();
-        
+		var recordId = component.get("v.recordId");
+		window.location.replace( "/" + recordId);  
 	}
 })

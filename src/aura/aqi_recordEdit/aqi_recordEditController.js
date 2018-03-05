e5ea76edaa58c09a57272636a2e40082bee359d5
faldecosea <faldecosea@altimetrik.com>
2018-03-05 17:04:40 -0300

    Resolved #651 - Change wrapper cmp name to something more meaningful

--- src/aura/aqi_recordEdit/aqi_recordEditController.js
+++ src/aura/aqi_recordEdit/aqi_recordEditController.js
@@ -1,28 +1,17 @@
 ({
 	save : function(component, event, helper) {
-        component.set("v.isErrorOnSave", true); 
+        component.set("v.isErrorOnSave", true);
     	component.find("edit").get("e.recordSave").fire();
 	},
- 
-    cancel : function(component, event,helper) { 
-       
+
+    cancel : function(component, event,helper) {  
          helper.backToRecord(component);
-        
+
     },
-    handleSaveSuccess : function(component, event,helper) { 
-        component.set("v.isErrorOnSave", false); 
-        // because there were no errors 
-        // // handle save success 
-  		console.log('\n ==== handleSaveSuccess ');
-         helper.backToRecord(component);
-        
+    handleSaveSuccess : function(component, event,helper) {
+        component.set("v.isErrorOnSave", false);
+        helper.backToRecord(component);
     },
     handleDoneWaiting : function(component, event,helper) {
-        // if there were errors during save,
-        //  the handleSaveSuccess doesn’t get fired 
-        //  if(component.get(“v.isErrorOnSave”) === true) { 
-        //  // show the error div 
-        //  }
-        console.log('\n ==== handleDoneWaiting ');
     }
 })

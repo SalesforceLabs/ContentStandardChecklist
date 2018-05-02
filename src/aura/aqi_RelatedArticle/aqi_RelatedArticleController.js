91ef87bc3db23a11ba9bb6736da582dd2ca0b237
faldecosea <faldecosea@altimetrik.com>
2018-05-02 15:37:45 -0300

    Design change and redirection to related article record page

--- src/aura/aqi_RelatedArticle/aqi_RelatedArticleController.js
+++ src/aura/aqi_RelatedArticle/aqi_RelatedArticleController.js
@@ -2,7 +2,19 @@
 	myAction : function(component, event, helper) {
 
     },
+	navigateTo : function(component, event, helper) {
 
+	    //Find the text value of the component with aura:id set to "address"
+
+		var idKav = component.get("v.kavId");
+		var sObectEvent = $A.get("e.force:navigateToSObject");
+		  sObectEvent .setParams({
+		  "recordId": idKav,
+		  "slideDevName": "detail"
+		 });
+		 sObectEvent.fire();
+
+	},
     doInit: function(cmp, event, helper) {
 
         helper.doInit(cmp, event, helper);
